f4d5660911bbe57a96e95102ab2abdc64b576458
apolloclark <apolloclark@gmail.com>
2019-03-10 16:34:41 -0400

    added docker push commands

--- build_packer_docker.sh
+++ build_packer_docker.sh
@@ -13,21 +13,26 @@ echo "$PACKAGE_VERSION"
 docker image rmi $DOCKER_USERNAME/$PACKAGE:$(date -u '+%Y%m%d') -f  || true
 docker image rmi $DOCKER_USERNAME/$PACKAGE:$PACKAGE_VERSION -f  || true
 docker image rmi $DOCKER_USERNAME/$PACKAGE -f  || true
-
 docker container rm default -f || true
 
 # run Packer
 packer validate packer_docker.json
-
 packer inspect packer_docker.json
-
 packer build packer_docker.json
+
+end=`date +%s`
+secs=$((end-start))
+printf 'runtime = %02dh:%02dm:%02ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60))
 
 # test the Docker image w/ Serverspec
 #rspec ./spec/Dockerfile_ubuntu18.04.rb
 #rspec ./spec/Dockerfile_centos7.4.rb
 rspec ./spec/Dockerfile_amzlinux1.rb
 
+# push images
+docker push $DOCKER_USERNAME/$PACKAGE:$(date -u '+%Y%m%d');
+docker push $DOCKER_USERNAME/$PACKAGE:$PACKAGE_VERSION;
+docker push $DOCKER_USERNAME/$PACKAGE;
 
 end=`date +%s`
 secs=$((end-start))
