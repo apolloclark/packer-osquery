303852712fa760150e8b9ab7b199ff087516c1dc
apolloclark <apolloclark@gmail.com>
2019-03-15 08:57:46 -0400

    renamed tests

--- build_packer_docker.sh
+++ build_packer_docker.sh
@@ -2,18 +2,18 @@
 start=`date +%s`
 
 # ensure that ENV VARs are set
+export DOCKER_BASE_IMAGE=${DOCKER_BASE_IMAGE:="amazonlinux:1"}
 export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
-echo "$DOCKER_USERNAME"
 export PACKAGE=${PACKAGE:=osquery}
-echo "$PACKAGE"
 export PACKAGE_VERSION=${PACKAGE_VERSION:=3.3.2-1.linux}
-echo "$PACKAGE_VERSION"
 
 # remove previously built local images
 docker image rmi $DOCKER_USERNAME/$PACKAGE:$(date -u '+%Y%m%d') -f  || true
 docker image rmi $DOCKER_USERNAME/$PACKAGE:$PACKAGE_VERSION -f  || true
 docker image rmi $DOCKER_USERNAME/$PACKAGE -f  || true
 docker container rm default -f || true
+
+
 
 # run Packer
 packer validate packer_docker.json
@@ -24,10 +24,10 @@ end=`date +%s`
 secs=$((end-start))
 printf 'runtime = %02dh:%02dm:%02ds\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60))
 
+
+
 # test the Docker image w/ Serverspec
-#rspec ./spec/Dockerfile_ubuntu18.04.rb
-#rspec ./spec/Dockerfile_centos7.4.rb
-rspec ./spec/Dockerfile_amzlinux1.rb
+rspec ./spec/Dockerfile_$DOCKER_BASE_IMAGE.rb
 
 # push images
 docker push $DOCKER_USERNAME/$PACKAGE:$(date -u '+%Y%m%d');
